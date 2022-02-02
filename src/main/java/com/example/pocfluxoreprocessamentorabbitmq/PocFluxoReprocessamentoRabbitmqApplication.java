a9b75381eafafacf02543c9802f2d30646494f05
Victor <victor.alvesf7@gmail.com>
2022-02-01 22:12:02 -0400

    base commit

---
+++ src/main/java/com/example/pocfluxoreprocessamentorabbitmq/PocFluxoReprocessamentoRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package com.example.pocfluxoreprocessamentorabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocFluxoReprocessamentoRabbitmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocFluxoReprocessamentoRabbitmqApplication.class, args);
+    }
+
+}
